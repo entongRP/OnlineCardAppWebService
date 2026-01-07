d5e43cbf1eeb46b30540aa21ef91d5bb07849154
entongRP <24012306@myrp.edu.sg>
2026-01-07 14:56:23 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+const port = 3000;
+
+//database configuration info
+const dbConfig = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: Number(port),
+    waitForConnections: true,
+    connectionLimit: 100,
+    queueLimit: 0,
+};
+
+//initialise express app
+const app = express();
+//help us read json
+app.use(express.json());
+
+//starting server
+app.listen(port, () => console.log(`Server started on port ${port}`));
+
+//get all cards
+app.get('/allcards', async (req, res) => {
+    try{
+        let connection = await mysql.createConnection(dbConfig);
+        const [rows] = await connection.execute('SELECT * FROM defaultdb.cards');
+        res.json(rows);
+    } catch(err) {
+        console.error(err);
+        res.status(500).json({message: 'Server error for allcards'});
+    }
+});
